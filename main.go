5ce0cab4502279f0badc7128951c11f004d2c6ff
Jorge Eastman <jeastman19@gmail.com>
2018-11-01 02:53:11 -0300

    Add Named Returns function

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ func Names() (string, string) {
 	return "Foo", "Bar"
 }
 
+func NamedReturn() (first string, second string) {
+	first = "First"
+	second = "Second"
+	return
+}
+
 func main() {
 	n1, n2 := Names()
 	fmt.Println(n1, n2)
@@ -15,4 +21,8 @@ func main() {
 	// The underscore character (_) is used to ignore returned value
 	n3, _ := Names()
 	fmt.Println(n3)
+
+	// Test Named returns
+	f, s := NamedReturn()
+	fmt.Println(f, s)
 }
